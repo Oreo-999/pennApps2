0fea7bd27e5d1b7419c948eec4e61ee000cf32a0
Oreo-999 <aryanfundu@gmail.com>
2025-09-20 10:10:10 -0400

    Initial Commit

---
+++ pennApps2/pennApps2App.swift
@@ -0,0 +1,17 @@
+//
+//  pennApps2App.swift
+//  pennApps2
+//
+//  Created by Aryan Sharma on 9/20/25.
+//
+
+import SwiftUI
+
+@main
+struct pennApps2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ pennApps2Tests/pennApps2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  pennApps2Tests.swift
+//  pennApps2Tests
+//
+//  Created by Aryan Sharma on 9/20/25.
+//
+
+import Testing
+@testable import pennApps2
+
+struct pennApps2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
